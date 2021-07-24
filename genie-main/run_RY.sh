32575756d53259eaf1831a243f87735027ef4c0c
Rui Ying <rui.ying@bristol.ac.uk>
2021-07-24 16:08:41 +0100

    Add a new wrap shell script to run model conviniently

--- genie-main/run_RY.sh
+++ genie-main/run_RY.sh
@@ -1,4 +1,5 @@
 for (( COUNTER=$1; COUNTER<=$2; COUNTER+=1 )); do
-    echo "Runing model: ForamECOGEM-", $COUNTER
-    qsub -j y -o cgenie_log -V -S /bin/bash runmuffin.sh muffin.CBE.worlg4.BASESFeTDTL RUI_LABS/sensitivity ForamECOGEM-$COUNTER.config 150 Grigoratou.8P7Z1F.SPIN
+    echo "Submitting model: No. $COUNTER"
+    qsub -j y -o cgenie_log -V -S /bin/bash runmuffin.sh muffin.CBE.worlg4.BASESFeTDTL \
+	 RUI_LABS/sensitivity ForamECOGEM-$COUNTER.config 150 Grigoratou.8P7Z1F.SPIN
 done
