caa7d7a9c19c8c501db2e435a0dea5259d2a7c9d
Rui Ying <rui.ying@bristol.ac.uk>
2021-12-14 18:46:44 +0000

    Add foram timeseries sites; modified Run_RY.sh

--- genie-main/run_RY.sh
+++ genie-main/run_RY.sh
@@ -1,5 +1,10 @@
 for (( COUNTER=$1; COUNTER<=$2; COUNTER+=1 )); do
     echo "Submitting model: No. $COUNTER"
-    qsub -j y -o cgenie_log -V -S /bin/bash runmuffin.sh muffin.CBE.worjh2.BASESFeTDTL.Albani\
-	 RUI_LABS/sensitivity_Albani ForamECOGEM-$COUNTER.config 300 test-64.config
+    qsub -j y -o cgenie_log -V -S /bin/bash runmuffin.sh muffin.CBE.worlg4.BASESFeTDTL\
+	 RUI_LABS/Sensitivity ForamECOGEM-$COUNTER.config 500 8P7Z4F_respir_scheme
+
+    ## For Albani basic config
+    # qsub -j y -o cgenie_log -V -S /bin/bash runmuffin.sh muffin.CBE.worjh2.BASESFeTDTL.Albani.config\
+    # 	 RUI_LABS/Sensitivity ForamECOGEM-$COUNTER.config 500 8P7Z4F_respir_scheme_Albani
+
 done
